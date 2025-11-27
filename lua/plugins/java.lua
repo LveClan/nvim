5b0acc1630a23ef24df263e89dee66113957a676
lilischao <lilischao@foxmail.com>
2025-11-27 18:12:04 +0800

    chore: 调试 nvim java  启动器

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -2,12 +2,28 @@ return {
   -- 加载官方的配置
   { import = "lazyvim.plugins.extras.lang.java" },
 
+  { import = "lazyvim.plugins.extras.dap.core" },
+
   {
     "mfussenegger/nvim-jdtls",
     opts = {
+      init_options = {
+        bundles = {
+          -- java-debug-adapter（保持不变）
+          vim.fn.glob(
+            vim.fn.stdpath("data")
+              .. "/mason/packages/java-debug-adapter/extension/server/com.microsoft.java.debug.plugin-*.jar",
+            true,
+            true
+          )[1],
+          -- java-test（关键！改成新路径）
+          vim.fn.glob(vim.fn.stdpath("data") .. "/mason/packages/java-test/extension/server/*.jar", true, true)[1],
+        },
+      },
       settings = {
         java = {
           maven = {
+            -- 设置maven路径
             settings = vim.fn.expand("~/.m2/settings.xml"),
           },
         },
