e632d9dde09764d23fd3e980b94498aab6a1021d
lilischao <lilischao@foxmail.com>
2025-12-04 17:29:41 +0800

    修改java 的默认注释风格为4个空格

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,3 +1,13 @@
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "java",
+  callback = function()
+    vim.opt_local.shiftwidth = 4
+    vim.opt_local.tabstop = 4
+    vim.opt_local.softtabstop = 4
+    vim.opt_local.expandtab = true
+  end,
+})
+
 return {
   -- 加载官方的配置
   { import = "lazyvim.plugins.extras.lang.java" },
